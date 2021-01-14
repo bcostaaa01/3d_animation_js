b82fe026a481c988d0bfa7881cb4e4bad9f59e39
Bruno <bcostinha01@gmail.com>
2021-01-14 13:54:35 +0000

    Update app.js and style.css
    Update app.js
    -add container eventListener
    
    Update style.css
    - add perspective to body
    - add transform-style to .card

--- app.js
+++ app.js
@@ -3,3 +3,10 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 
+// moving animation event
+
+container.addEventListener('mousemove', (e) => {
+     let xAxis = ((window.innerWidth / 2 - e.pageX) / 10);
+     let yAxis = ((window.innerHeight / 2) - e.pageY / 10);
+     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+});
