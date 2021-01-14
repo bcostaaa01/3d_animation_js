b5c6f8f9d0c62c07de4686273c9bc999f6a91dfd
Bruno <bcostinha01@gmail.com>
2021-01-14 14:01:28 +0000

    Update app.js
    Add animate card in eventListener
    
    Add animate card out eventListener

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const container = document.querySelector('.container');
 
 container.addEventListener('mousemove', (e) => {
      let xAxis = ((window.innerWidth / 2 - e.pageX) / 10);
-     let yAxis = ((window.innerHeight / 2) - e.pageY / 10);
+     let yAxis = ((window.innerHeight / 2 - e.pageY) / 10);
      card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 });
+
+// animate card in
+
+container.addEventListener('mouseenter', (e) => {
+    card.style.transition = 'none';
+})
+
+// animate card out
+
+container.addEventListener('mouseleave', (e) => {
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateY(0deg)`;
+})
