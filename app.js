269eadbd970d9b6a4a6c802b3df64cf1b1efb522
Bruno <bcostinha01@gmail.com>
2021-01-14 14:51:53 +0000

    Update app.js, index.html, style.css
    Update app.js
    - add title.style.transform to container.addEventListener
    - corrected errors
    
    Update index.html
    - added lacking 1.0 to initial-scale
    - added lacking <div> closing tag
    
    Update style.css
    -added transition to .phone img, .info h1, .info h3, .storages, .purchase
    - corrected syntax

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const container = document.querySelector('.container');
 // items
 const title = document.querySelector('.title');
 const phone = document.querySelector('.phone img');
-const purchase = document.querySelector('.purchase button');
+const purchase = document.querySelector('.purchase');
 const description = document.querySelector('.info h3');
 const storages = document.querySelector('.storages');
 
@@ -13,20 +13,24 @@ const storages = document.querySelector('.storages');
 // moving animation event
 
 container.addEventListener('mousemove', (e) => {
-     let xAxis = ((window.innerWidth / 2 - e.pageX) / 10);
-     let yAxis = ((window.innerHeight / 2 - e.pageY) / 10);
-     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+     let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+     let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
 // animate card in
 
 container.addEventListener('mouseenter', (e) => {
     card.style.transition = 'none';
+    // popout effect
+    title.style.transform = 'translateZ(150px)';
 })
 
 // animate card out
 
 container.addEventListener('mouseleave', (e) => {
     card.style.transition = 'all 0.5s ease';
-    card.style.transform = `rotateY(0deg) rotateY(0deg)`;
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    // popback effect
+    title.style.transform = 'translateZ(0px)';
 })
